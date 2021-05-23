795d12abf1d632f5843c17338627b5d84673676a
Felix Wahlsberg <felix.wahlsberg@student.careeria.fi>
2021-05-23 21:57:21 +0300

    navigointi ok sivujen välillä

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import landingview
+from .views import landingview, productlistview, supplierlistview
 
 urlpatterns = [
     path('', landingview),
+    path('suppliers/', supplierlistview),
+    path('products/', productlistview)
 ]
